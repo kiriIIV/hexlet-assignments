e0edc919df055b4a6040d63ef83826dd1c4eb219
@hexlet/cli <support@hexlet.io>
2025-03-03 02:46:58 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -1,9 +1,5 @@
 package exercise;
 
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
 // BEGIN
 public class App {
     public static void swapKeyValue(KeyValueStorage storage) {
